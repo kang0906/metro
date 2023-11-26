4931bf87758cbca6ba238e69e425b09a7bee6ac8
jinkoo kang <jinkoo906@gmail.com>
2023-11-26 19:29:08 +0900

    프로젝트 생성

---
+++ metro/src/test/java/com/example/metro/MetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
