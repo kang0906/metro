6913d0a03e6532a1cfd61b7ef691c0bc896a7c14
jinkoo kang <jinkoo906@gmail.com>
2023-12-09 10:37:22 +0900

    repository 추가

---
+++ metro/src/main/java/com/example/metro/map/repository/SubwayStationRepository.java
@@ -0,0 +1,7 @@
+package com.example.metro.map.repository;
+
+import com.example.metro.map.entity.SubwayStation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubwayStationRepository extends JpaRepository<Long, SubwayStation> {
+}
---
+++ metro/src/main/java/com/example/metro/map/repository/SubwayTrackRepository.java
@@ -0,0 +1,7 @@
+package com.example.metro.map.repository;
+
+import com.example.metro.map.entity.SubwayTrack;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubwayTrackRepository extends JpaRepository<Long, SubwayTrack> {
+}
